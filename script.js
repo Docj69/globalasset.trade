adc0114112cb2fc56a3fb74a030bd92acd57da57
Docj69 <007.niro@pm.me>
2025-05-19 22:59:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
   const input = document.getElementById('passwordInput').value;
-  const correct = "GAM2024"; // Cambia questa password
+  const correct = "Alive5-Greasily8-Finalist2-Drift4-Five5"; // Cambia questa password
   if (input === correct) {
     window.location.href = "private.html";
   } else {
